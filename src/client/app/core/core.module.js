e119b5ad76daee66fd4dd3c0b40a72eda7af01fa
jonyeezs <jonyeezs@gmail.com>
2017-03-05 00:02:24 +1000

    update angular version and remove the ugly overlay spinner

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -5,6 +5,6 @@
       .module('app.core', [
           'ngAnimate', 'ngSanitize',
           'blocks.exception', 'blocks.logger', 'blocks.router',
-          'ui.router', 'ui.bootstrap', 'ngplus'
+          'ui.router', 'ui.bootstrap'
       ]);
 })();
