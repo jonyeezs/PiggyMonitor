c6e259f2ae44f47cc41b6bf88dbeac8eb60ee5b2
jonyeezs <jonyeezs@gmail.com>
2017-03-06 22:47:10 +1000

    indicate when async dropdown is loading

--- src/client/app/widgets/drop-options/drop-options.component.js
+++ src/client/app/widgets/drop-options/drop-options.component.js
@@ -18,6 +18,19 @@
       ctrl.selected = ctrl.selectionInfo;
     };
 
+    ctrl.$onChanges = function(changes) {
+      if (changes.availableOptions
+          && (!changes.availableOptions.currentValue || changes.availableOptions.currentValue.length === 0)
+          && changes.availableOptions.isFirstChange()) {
+        ctrl.noList = true;
+      }
+
+      if (changes.availableOptions
+          && (changes.availableOptions.currentValue && changes.availableOptions.currentValue.length > 0)) {
+        ctrl.noList = false;
+      }
+    }
+
     ctrl.select = function (option) {
       ctrl.selected = option.key;
       ctrl.onSelect({ value: option.value });
