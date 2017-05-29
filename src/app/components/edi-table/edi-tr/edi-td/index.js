b25f5ed138e587f60993caaa62735b20f1b66c5e
Jonyeezs <jonyeezs@gmail.com>
2017-05-29 21:46:36 +1000

    register missing dependency

--- src/app/components/edi-table/edi-tr/edi-td/index.js
+++ src/app/components/edi-table/edi-tr/edi-td/index.js
@@ -1,4 +1,6 @@
 module.exports = angular
-  .module('edi-tr.edi-td', []).name;
+  .module('edi-tr.edi-td', [
+    require('../../../../common/filters/removeNegSym')
+  ]).name;
 
 require('./edi-td.component');
