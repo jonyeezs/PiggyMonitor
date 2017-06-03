d9098162104553b392971ddcc6f0429d3f07b761
Jonyeezs <jonyeezs@gmail.com>
2017-06-03 21:43:01 +1000

    add some updates to karma

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'specs/**/*.spec.js': ['webpack']
+      'specs/**/*.spec.js': ['webpack', 'sourcemap']
     },
 
     // test results reporter to use
@@ -60,7 +60,8 @@ module.exports = function(config) {
     client: {
       mocha: {
         // change Karma's debug.html to the mocha web reporter
-        reporter: 'html'
+        reporter: 'html',
+        fullTrace: true
       }
     },
     // start these browsers
@@ -78,7 +79,7 @@ module.exports = function(config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
     // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.debug ? config.LOG_ERROR : config.LOG_DISABLE,
+    logLevel: config.debug ? config.LOG_DISABLE : config.LOG_ERROR,
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: config.debug ? true : false,
 
